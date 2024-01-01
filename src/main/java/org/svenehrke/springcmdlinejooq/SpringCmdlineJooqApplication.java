d02159d8a6ce207e6ec616673465c45fb7353b26
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2024-01-01 09:23:23 +0100

    setup for commandline app

--- src/main/java/org/svenehrke/springcmdlinejooq/SpringCmdlineJooqApplication.java
+++ src/main/java/org/svenehrke/springcmdlinejooq/SpringCmdlineJooqApplication.java
@@ -1,13 +1,18 @@
 package org.svenehrke.springcmdlinejooq;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCmdlineJooqApplication {
+public class SpringCmdlineJooqApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCmdlineJooqApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("RUNNING");
+	}
 }
